569e19857193bbd8ecdc15956009264bb9710e5f
mudassir-ahmed000 <159802093+mudassir-ahmed000@users.noreply.github.com>
2024-02-22 14:06:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,145 @@
+// there are three ways to get output in browser
+// 1) alert()
+    //   alert(10); 
+// 2) document.write()
+    //    document.write(10);
+// 3) console.log()
+    //   console.log(10);
+
+// document.write(typeof "mudassir" + "<br>")
+// document.write(typeof 50 + "<br>")
+// document.write(typeof true + "<br>")
+
+
+//     var a = 18 ;
+//     var firstname = "Mudassir"
+//     var lastname = "Ahmed"
+//     // console.log(typeof a, typeof firstname, typeof lastname )
+//     // console.log(a)
+//     // console.log(firstname + lastname)
+//     // console.log(firstname + " " + lastname)
+//     // console.log(firstname)
+//     // console.log(lastname)
+
+//     document.write("<b> My firstname is </b>: ", firstname  , "<br>")
+//     document.write("My lastname is : ", "<i>" +lastname+ "</i>" , "<br>")
+//     document.write("<u>My age is</u> : ", a )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // chapter # 1
+
+    // Q1)  <script src="app.js"></script>
+    // Q2)  alert(Error! pleas enter a valid password)
+    // Q3)  alert("Welcome to JS land .... \n  Happy Coding!")
+    // Q4)  alert("Welcome to JS land ....")
+
+
+    // chapter # 2
+
+    //Q1)  var username;
+    //Q2)  var myName = "Mudassir Ahmed";
+    //Q3)  var message = "Hello World!";
+    //     alert(message);
+    //Q4)  var myName = "Mudassir Ahmed";
+    //     alert(myName);
+    //     var age = "I am 17 year old";
+    //     alert(age);
+    //     var course = "Web Development";
+    //     alert(course);
+    //Q5)  var pizza = "PIZZA" + "\n" + "PIZZ" + "\n" + "PIZ" + "\n" + "PI" + "\n" + "P" + "\n"
+    //     alert(pizza);
+    //Q6)  var email = "imudassirahmed2006@gmail.com";
+    //     alert("My email address is " + email);
+    //Q7)  var book = "A smarter way to learn JavaScript."
+    //     alert("I am trying to learn from the book " + book)
+    //Q8)  var message = "Yah! Ican write HTML content through JavaScript" ;
+    //     document.write(message);
+    //Q9)  var symbol = "▬▬▬▬▬▬▬▬▬" +  "ஜ۩۞۩ஜ" + "▬▬▬▬▬▬▬▬▬";
+    //     alert(symbol);
+    //     document.write(symbol);
+
+
+    // chapter # 3
+
+    //Q1)  var age = 17
+    //     alert("I am" + " " + age + " " + "years old.")
+    //Q3)  var birthYear = 2006;
+    //     document.write("My birth year is " + birthYear + "<br>" + "Data type of my declared Variable is " + typeof(birthYear)) ;
+    //Q4)  var visitorName = "John Doe";
+    //     var productTitle = "T-shirts";
+    //     var quantity = 5 ;
+    //     document.write("<b>" + visitorName + "</b>" + " " + "ordered" + " " + "<b>" + quantity + "</b>" + " " + "<b>" + productTitle + "</b>" + " " + "on XYZ clothing store.")
+      
+
+    // PDF
+
+    // chapter # 1
+
+    //Q1)  var firstName = "Mudassir";
+    //     alert("My first name is " + firstName);
+    //     var lastName = "Ahmed";
+    //     alert("My last name is " + lastName);
+    //     var email = "imudassirahmed2006@gmail.com";
+    //     alert("My email is " + email);
+    //     var phoneNumber = "+" + 92 +" "+ 3358174709 ;
+    //     alert("My phone number is " + phoneNumber);
+ 
+    //Q2)  alert("You are learning JavaScript!")
+
+    //Q3)  alert("JavaScript is very HArd!")
+
+    // chapter # 2
+
+    //Q1)  var camelCase = "Mudassir Ahmed";
+
+    //Q2)  var myChoice;
+    //     myChoice = "Jawan Pakistan" ;
+
+    //Q3)  var teamName = "Warrior FC";
+    //     alert(teamName);
+
+    //Q4) var bestMan = "Charlie";
+    //    var bestMan = "Michael Jackson";
+
+
+    // chapter # 3
+
+    //Q1)  var caseQty ;
+  
+    //Q2)  var caseQty = 144 ;
+
+    //Q3)  var num = "9";
+    //     var num = 9;
+
+    //Q4)  var sum;
+    //     sum = 2 + 2;
+    //     alert(sum);
+
+    //Q5)  var merchTotal = 100;
+    //     var shippingCharge = 10;
+    //     var orderTotal = merchTotal + shippingCharge; 
+    //     document.write("The value of order total is " + orderTotal );
+
+
+    //Q6)  var firstNum = 5;
+    //     firstNum = firstNum + 5;
+    //     document.write(firstNum);
